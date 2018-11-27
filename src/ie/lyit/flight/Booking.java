0769ab3b5489565f4d8a73651c70bf647f80235a
L00149486 <L00149486@LYLIB1-02.lyitnet.lyit.ie>
2018-11-27 11:23:43 +0000

    booking class work

--- src/ie/lyit/flight/Booking.java
+++ src/ie/lyit/flight/Booking.java
@@ -3,12 +3,13 @@ package ie.lyit.flight;
 import java.util.ArrayList;
 
 public class Booking {
-	Flight outbound;
-	Flight inbound;
-	ArrayList<Passenger> passengers;
-	double totalPrice;
+	private Flight outbound;
+	private Flight inbound;
+	private ArrayList<Passenger> passengers;
+	private double totalPrice;
 	static int count;
 	
+	///CONSTRUCTORS
 	public Booking()
 	{
 		
@@ -20,17 +21,56 @@ public class Booking {
 		this.inbound = iF;
 		this.passengers = p;
 	}
-	/*
-	toString():String
-	equals(
-	obj
-	:
-	Object
-	):boolean
-	findPassenger(
-	p
-	:Passenger
-	):boolean
-	calculate
-	Price():double*/
+	
+	
+	///GETTER/SETTERS
+	public void setOutbound(Flight oF){this.outbound = oF;}
+	
+	public Flight getOutbound(){return this.outbound;}
+	
+	public void setInbound(Flight iF){this.inbound = iF;}
+	
+	public Flight getInbound(){return this.inbound;}
+	
+	public void setPassengers(ArrayList<Passenger> p){this.passengers = p;}
+	
+	public ArrayList<Passenger> getPassengers(){return this.passengers;}
+	
+	public double getTotalPrice(){return this.totalPrice;}
+	
+	public void setTotalPrice(){this.totalPrice=this.calculatePrice();}
+	
+	
+	
+	///SPECIFIC METHODS
+	@Override
+	public String toString()
+	{
+		return ("Booking flight from "+inbound.toString()+" to "+inbound.toString()+" Price: "+totalPrice);
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+	   Booking pObject;
+	   if (obj instanceof Booking)
+		   pObject = (Booking)obj;
+	   else
+	       return false;
+	 
+	   return super.equals(pObject)
+	          && this.inbound == pObject.inbound
+	          && this.outbound == pObject.outbound;
+	}
+	
+	public boolean findPassenger(Passenger p)
+	{
+		for (Passenger tempP : this.passengers)
+			if(tempP.equals(p)) return true;
+		return false;
+	}
+
+	private double calculatePrice()
+	{
+		return (outbound.getPrice()+inbound.getPrice())*count;
+	}
 }
