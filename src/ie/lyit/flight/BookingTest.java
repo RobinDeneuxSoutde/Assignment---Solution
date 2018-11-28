f5d60f231d0733c87b94537d3c7b879067d67a9c
L00149486 <L00149486@PR2409-21.lyitnet.lyit.ie>
2018-11-28 11:27:01 +0000

    BoingTest

---
+++ src/ie/lyit/flight/BookingTest.java
@@ -0,0 +1,54 @@
+package ie.lyit.flight;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookingTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBookingFlightFlightArrayListOfPassenger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOutbound() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetInbound() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassengers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTotalPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindPassenger() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCalculatePrice() {
+		fail("Not yet implemented");
+	}
+
+}
